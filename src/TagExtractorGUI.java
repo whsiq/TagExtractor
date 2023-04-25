0e184fc712afae7ab6ab8c31c56c902edc8b60bc
whsiq <staggsce@mail.uc.edu>
2023-04-25 11:15:14 -0400

    Built Swing GUI

--- src/TagExtractorGUI.java
+++ src/TagExtractorGUI.java
@@ -1,2 +1,72 @@
-package PACKAGE_NAME;public class TagExtractorGUI {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class TagExtractorGUI extends JFrame {
+    JPanel mainPnl;
+    JPanel filePnl;
+    JPanel displayPnl;
+    JPanel optionPnl;
+
+    JTextField browseTF;
+    JButton browseBtn;
+
+    JTextArea tagTA;
+    JScrollPane scroller;
+
+    JButton quitBtn;
+    JButton saveBtn;
+
+    public TagExtractorGUI() {
+        mainPnl = new JPanel();
+        mainPnl.setLayout(new BorderLayout());
+
+        createFilePanel();
+        mainPnl.add(filePnl, BorderLayout.NORTH);
+
+        createDisplayPanel();
+        mainPnl.add(displayPnl, BorderLayout.CENTER);
+
+        createOptionPanel();
+        mainPnl.add(optionPnl, BorderLayout.SOUTH);
+
+        add(mainPnl);
+        setSize(400, 400);
+        setTitle("Tag Extractor");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void createFilePanel() {
+        filePnl = new JPanel();
+        filePnl.setLayout(new BorderLayout());
+
+        browseTF = new JTextField();
+        browseBtn = new JButton("Browse..");
+
+        filePnl.add(browseTF, BorderLayout.CENTER);
+        filePnl.add(browseBtn, BorderLayout.EAST);
+    }
+
+    public void createDisplayPanel() {
+        displayPnl = new JPanel();
+        displayPnl.setLayout(new GridLayout(1,1));
+
+        tagTA = new JTextArea();
+        scroller = new JScrollPane(tagTA);
+
+        displayPnl.add(scroller);
+    }
+
+    public void createOptionPanel() {
+        optionPnl = new JPanel();
+        optionPnl.setLayout(new GridLayout(1,2));
+
+        saveBtn = new JButton("Save");
+        quitBtn = new JButton("Quit");
+        quitBtn.addActionListener((ActionEvent ae) -> System.exit(0));
+
+        optionPnl.add(saveBtn);
+        optionPnl.add(quitBtn);
+    }
 }
---
+++ src/TagExtractorGUIRunner.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class TagExtractorGUIRunner extends JFrame {
+    public static void main(String[] args) {
+        JFrame frame = new TagExtractorGUI();
+    }
+}
